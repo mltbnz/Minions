b89d59cca6548de2b8d68f22fb946af4f112d930
Malte Bünz <mb@clapp.de>
2016-06-27 18:53:57 +0200

    First Classes

---
+++ Minions/CMAsyncHelper.swift
@@ -0,0 +1,35 @@
+//
+//  CMAsyncHelper.swift
+//  
+//
+//  Created by Malte Bünz on 23/06/16.
+//
+//
+
+import Foundation
+
+/// Global Helper Functions
+/**
+ Executes the closure on the main queue after a set amount of seconds.
+ 
+ - parameter delay:   Delay in seconds
+ - parameter closure: Code to execute after delay
+ */
+func delayOnMainQueue(delay: Double, clousure: () -> ()) {
+    let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)))
+    let queue = dispatch_get_main_queue()
+    dispatch_after(dispatchTime, queue, clousure)
+}
+
+/**
+ Executes the closure on a background queue after a set amount of seconds.
+ 
+ - parameter delay:   Delay in seconds
+ - parameter closure: Code to execute after delay
+ */
+func delayOnBackgroundQueue(delay: Double, closure: () -> ()) {
+    let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)))
+    let qos = QOS_CLASS_UTILITY
+    let queue = dispatch_get_global_queue(qos, 0)
+    dispatch_after(dispatchTime, queue, closure)
+}
